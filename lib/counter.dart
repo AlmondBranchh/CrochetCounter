9ed99f3853e048ffa2d4c5383d0dea9519c401f5
AlmondBranchh <96097095+AlmondBranchh@users.noreply.github.com>
2022-05-13 11:45:23 -0500

    Reformatted to have multiple .dart files

---
+++ lib/counter.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+int counter = 0;
+
+class CounterSection extends StatefulWidget {
+  const CounterSection({Key? key}) : super(key: key);
+
+  @override
+  State<CounterSection> createState() => _CounterSectionState();
+}
+
+class _CounterSectionState extends State<CounterSection> {
+  @override
+  Widget build(BuildContext context) {
+    void _counterSub(){
+      setState((){
+        counter --;
+      });
+    }
+    void _counterAdd(){
+      setState((){
+        counter ++;
+      });
+    }
+    void _counterClear(){
+      setState((){
+        counter = 0;
+      });
+    }
+
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(top: 50),
+      child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.min ,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            fixedSize: const Size(65, 65),
+            shape: const CircleBorder(),
+          ),
+
+          onPressed: _counterSub,
+          child: const Icon(Icons.arrow_back_ios_rounded),
+
+
+        ),ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size(250, 250),
+            shape: const CircleBorder(),
+          ),
+
+          onPressed: _counterAdd,
+          child: Text(
+            "$counter",
+            style: TextStyle(fontSize: 64),
+          ),
+
+
+        ),ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              fixedSize: const Size(50, 50),
+              shape: const CircleBorder(),
+            ),
+
+            onPressed: _counterClear,
+            child: Icon(Icons.clear)
+        ),
+        ],
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:crochet_counter/counter.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-int counter = 0;
-int timerSeconds = 0;
-int timerMinutes = 0;
-int timerHours = 0;
-//branchOffAdded
-//This is branch off added
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -23,101 +17,11 @@ class MyApp extends StatelessWidget {
           title: const Text("Project name"),
         ),
         body: Column(
-          children: [
-            CounterWidget,
+          children: const [
+            CounterSection(),
           ],
         )
       ),
-    );
-  }
-}
-
-//Starting widgets from the top of the page to the Bottom
-
-//Timer Widget
-
-
-Widget TimeWidget = Container(
-  child: Row(
-
-  ),
-);
-
-//Counter Widgets
-
-Widget CounterWidget = Container(
-  alignment: Alignment.center,
-  padding: const EdgeInsets.only(top: 50),
-  child: const CounterSection(),
-);
-
-
-//Counter stateful
-class CounterSection extends StatefulWidget {
-  const CounterSection({Key? key}) : super(key: key);
-
-  @override
-  State<CounterSection> createState() => _CounterSectionState();
-}
-
-class _CounterSectionState extends State<CounterSection> {
-  @override
-  Widget build(BuildContext context) {
-    void _counterSub(){
-      setState((){
-        counter --;
-      });
-    }
-    void _counterAdd(){
-      setState((){
-        counter ++;
-      });
-    }
-    void _counterClear(){
-      setState((){
-        counter = 0;
-      });
-    }
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      mainAxisSize: MainAxisSize.min ,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue,
-            fixedSize: const Size(65, 65),
-            shape: const CircleBorder(),
-          ),
-
-          onPressed: _counterSub,
-          child: const Icon(Icons.arrow_back_ios_rounded),
-
-
-        ),ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            fixedSize: const Size(250, 250),
-            shape: const CircleBorder(),
-          ),
-
-          onPressed: _counterAdd,
-          child: Text(
-            "$counter",
-            style: TextStyle(fontSize: 64),
-          ),
-
-
-        ),ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.red,
-            fixedSize: const Size(50, 50),
-            shape: const CircleBorder(),
-          ),
-
-          onPressed: _counterClear,
-          child: Icon(Icons.clear)
-        ),
-      ],
     );
   }
 }
