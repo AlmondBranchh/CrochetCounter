5a881a4c2996e5d2b1bcb3be9b21f2fd24b2008f
AlmondBranchh <96097095+AlmondBranchh@users.noreply.github.com>
2022-05-21 11:23:01 -0500

    Change the list view in project selector

--- lib/frontend_ui/screens/project_selector.dart
+++ lib/frontend_ui/screens/project_selector.dart
@@ -1,3 +1,4 @@
+import 'package:crochet_counter/frontend_ui/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:crochet_counter/frontend_ui/widgets/create_new_project.dart';
 import 'package:crochet_counter/logic/project.dart';
@@ -14,36 +15,86 @@ class _ProjectSelectorState extends State<ProjectSelector> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Projects"),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: (){
-              showDialog(
-                context: context,
-                builder: (BuildContext context){
-                  return NewProjectWindow();
-                }
-              ).then((value) => setState((){}));
-            },
-            child: Text("New Project"),
+        backgroundColor: Colors.transparent,
+        title: Text(
+            "Project Selector",
+          style: TextStyle(
+            color: secondColor
           ),
-          ListView.builder(
+        ),
+      ),
+      body: ListView.builder(
             itemCount: GetProjectList().length,
             shrinkWrap: true,
             itemBuilder: (context, index){
-              return ListTile(
-                title: Text(GetProjectList()[index].name),
-                onTap: (){
-                OpenProject(GetProjectList()[index]);
-                Navigator.pop(context);
-                },
-              );
+              return ListViewTile(context , index);
             }
-          )
-        ],
+          ),
+      floatingActionButton: FloatingActionButton.large(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return NewProjectWindow();
+              }
+          ).then((value) => setState(() {}));
+        },
+        backgroundColor: secondColor,
+        child: Icon(
+            Icons.add,
+          size: 82,
+          color: fourthColor,
+        ),
       ),
     );
   }
+}
+
+Widget ListViewTile(BuildContext context, int index){
+  return ListTile(
+    title: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                    GetProjectList()[index].name,
+                  style: const TextStyle(
+                    fontSize: 28,
+                    color: firstColor
+                  ),
+                ),
+                Text(
+                  "2:00:00",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: secondColor,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 375,
+            height: 1,
+            decoration: const BoxDecoration(
+                color: firstColor,
+                borderRadius: BorderRadius.all(Radius.circular(4))
+            ),
+          )
+        ],
+      ),
+    ),
+    onTap: (){
+      OpenProject(GetProjectList()[index]);
+      Navigator.pop(context);
+    },
+  );
 }
