499d04847714751fcdbff6c22c4c6ac87334c56d
AlmondBranchh <96097095+AlmondBranchh@users.noreply.github.com>
2022-05-26 22:28:07 -0500

    Added the delete button to project

--- lib/frontend_ui/screens/project_editor.dart
+++ lib/frontend_ui/screens/project_editor.dart
@@ -1,6 +1,7 @@
 import 'package:crochet_counter/logic/data.dart';
 import 'package:flutter/material.dart';
 import 'package:crochet_counter/logic/project.dart';
+import 'package:crochet_counter/frontend_ui/colors.dart';
 
 class ProjectEditor extends StatefulWidget {
   const ProjectEditor({Key? key}) : super(key: key);
@@ -13,14 +14,25 @@ class _ProjectEditorState extends State<ProjectEditor> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      backgroundColor: fourthColor,
       content: SizedBox(
-        child: ElevatedButton(
-          onPressed: (){
-      DeleteProject(SelectedProject(GetSelectedProjectIndex()));
-      Navigator.pop(context);
-           },
-          child: Text(
-            "Delete"
+        width: 250,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: thirdColor,
+            ),
+            onPressed: () {
+              DeleteProject(SelectedProject(GetSelectedProjectIndex()));
+              Navigator.pop(context);
+            },
+            child: const Text(
+              "Delete",
+              style: TextStyle(
+                  color: firstColor
+              ),
+            ),
           ),
         ),
       ),
